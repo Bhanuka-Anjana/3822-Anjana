54f142c47db5a3d44377e78c415d7eb7c535144e
Bhanuka <bhanuka2500@gmail.com>
2024-04-22 18:28:24 +0530

    create a pipeline for jenkins

---
+++ shop-frontend/pipeline.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+
+    environment {
+        DOCKER_IMAGE_NAME = 'shop-frontend'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Checkout the code from the Git repository
+                git 'http://gitlab.com/username/shop-frontend.git'
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                // Build the Docker image
+                script {
+                    docker.build(DOCKER_IMAGE_NAME)
+                }
+            }
+        }
+
+        stage('Run Docker Container') {
+            steps {
+                // Run the Docker container
+                script {
+                    docker.image(DOCKER_IMAGE_NAME).withRun('-p 3000:80')
+                }
+            }
+        }
+    }
+}
